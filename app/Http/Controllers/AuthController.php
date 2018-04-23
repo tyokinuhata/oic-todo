1005c1b64f182f3d1443cebe2ea6fb4da27898d4
Kazuki Tobita <kazukiti201@gmail.com>
2018-04-23 13:52:23 +0900

    登録用APIの作成

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
+/**
+ * 認証に関するコントローラ.
+ * サインイン, サインアップなどの制御を行う.
+ */
 class AuthController extends Controller {
+    /**
+     * ユーザID, パスワード, パスワード(確認)を取得する.
+     * 登録可能であれば登録して成功したことを通知し, 登録不可能であれば登録せずにエラーを返す.
+     */
     public function register(Request $request) {
+        // 入力されたパスワードとパスワード(確認)が正しいかどうか
+        if ($request->password === $request->confirmPassword) return response('Conflict', 409);
 
+        // まだ使用されていないユーザIDかどうか
+        $exist = User::where('user_id', $request->userId)->first();
+        if ($exist) return response('Authorized', 401);
+
+        return response('OK', 200);
     }
 
     public function token (Request $request) {
