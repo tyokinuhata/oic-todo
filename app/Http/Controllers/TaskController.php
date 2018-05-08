62f920ab45708b17f7556d3821434ab5d2cb6cb4
Kazuki Tobita <kazukiti201@gmail.com>
2018-05-09 02:00:20 +0900

    Update TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,19 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * タスク管理に関するコントローラ.
+ * タスクの追加, 一覧, 更新, 削除などを行う.
+ */
 class TaskController extends Controller {
+    /**
+     * タスクを追加する.
+     */
     public function add (Request $request) {
 
     }
 
+    /**
+     * タスク一覧を取得する.
+     */
     public function list (Request $request) {
 
     }
 
+    /**
+     * タスクを更新する.
+     */
     public function update (Request $request) {
 
     }
 
+    /**
+     * タスクを削除する.
+     */
     public function delete (Request $request) {
 
     }
