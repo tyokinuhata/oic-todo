9df3f97b90f3712e5c9f9230b778ef98479cc4dd
Kazuki Tobita <kazukiti201@gmail.com>
2018-05-14 16:09:15 +0900

    タスク追加APIの作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 /**
  * タスク管理に関するコントローラ.
@@ -13,7 +14,21 @@ class TaskController extends Controller {
      * タスクを追加する.
      */
     public function add (Request $request) {
+        // トークンに対応するユーザIDの取得
+        $user_id = User::select('user_id')->where('token', $request->token)->first();
+        if (!isset($user_id)) return response('Not Found', 404);
 
+        // 登録処理
+        DB::table('tasks')->insert([
+            'task_id' => uniqid(),
+            'name' => $request->name,
+            'description' => $request->description,
+            'state' => 'incomplete',
+            'score' => 0,
+            'user_id' => $user_id
+        ]);
+
+        return response('OK', 200);
     }
 
     /**
