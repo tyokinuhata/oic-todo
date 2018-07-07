19a925c080e12a9528e5eabe30e652801003deb9
Kazuki Tobita <kazukiti201@gmail.com>
2018-07-07 14:43:51 +0900

    不要な値を返却しないようにした

--- app/Task.php
+++ app/Task.php
@@ -9,4 +9,6 @@ class Task extends Model
     public $timestamps = false;
 
     protected $fillable = [ 'task_id', 'title', 'description', 'completed', 'score', 'user_id', 'reopen_at' ];
+
+    protected $hidden = [ 'id' ];
 }
